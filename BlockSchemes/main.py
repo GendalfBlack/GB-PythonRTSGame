7e8e76fce7e6e23d6510d2fefa94d3b981d539e8
GendalfBlack <garmash_a@itstep.academy>
2022-11-03 01:51:55 +0200

    Draw creates width and height with default values. Point in and out from basic blocks. Blocks dictionary. Now you can add blocks to one another to connect them with straight line with arrow pointed towards second object in operation.

--- BlockSchemes/main.py
+++ BlockSchemes/main.py
@@ -11,11 +11,18 @@ class Prog:
 
 
 class Vector2:
-    def __init__(self, x = 0, y = 0):
-        self.x = x
-        self.y = y
+    def __init__(self, x = None, y = None):
+        if x:
+            self.x = x
+        else:
+            self.x = 0
+        if y:
+            self.y = y
+        else:
+            self.y = 0
 
 class Block:
+    blocks = {}
     def __init__(self, **args):
         if "name" in args.keys():
             self.name = args["name"]
@@ -29,23 +36,51 @@ class Block:
         self.draw()
     def draw(self):
         Prog.c.create_text(self.pos.x, self.pos.y, text="?")
+    def __add__(self, other):
+        if self.point_out and other.point_in:
+            Prog.c.create_line(
+                self.point_out.x,
+                self.point_out.y,
+                other.point_in.x,
+                other.point_in.y,
+                arrow=LAST
+                )
+
 
 class Start(Block):
     def __init__(self, **args):
         super().__init__(name="start", text="start", **args)
+        Block.blocks[self.name] = self
     def draw(self):
-        Prog.c.create_oval(self.pos.x-50, self.pos.y-15, self.pos.x+50, self.pos.y+15,fill="white")
-        Prog.c.create_text(self.pos.x, self.pos.y, text=self.name)
+        self.width = 100
+        self.height = 30
+        self.shape = Prog.c.create_oval(
+            self.pos.x-self.width/2,
+            self.pos.y-self.height/2,
+            self.pos.x+self.width/2,
+            self.pos.y+self.height/2,
+            fill="white")
+        self.point_out = Vector2(self.pos.x,self.pos.y + self.height / 2)
+        self.text = Prog.c.create_text(self.pos.x, self.pos.y, text=self.name)
 
 class Finish(Block):
     def __init__(self, **args):
         super().__init__(name="finish", text="finish", **args)
+        Block.blocks[self.name] = self
     def draw(self):
-        Prog.c.create_oval(self.pos.x-50, self.pos.y-15, self.pos.x+50, self.pos.y+15,fill="white")
-        Prog.c.create_text(self.pos.x, self.pos.y, text=self.name)
+        self.width = 100
+        self.height = 30
+        self.shape = Prog.c.create_oval(
+            self.pos.x - self.width / 2,
+            self.pos.y - self.height / 2,
+            self.pos.x + self.width / 2,
+            self.pos.y + self.height / 2,
+            fill="white")
+        self.point_in = Vector2(self.pos.x,self.pos.y - self.height / 2)
+        self.text = Prog.c.create_text(self.pos.x, self.pos.y, text=self.name)
 
 Prog(w)
-Start(pos=Vector2(100,100))
-Finish(pos=Vector2(100,200))
-
+b1 = Start(pos=Vector2(100,100))
+b2 = Finish(pos=Vector2(100,200))
+b1+b2
 w.mainloop()
